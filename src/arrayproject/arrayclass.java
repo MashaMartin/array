fa8998ce0372bd19c51bd1fb3c698f17c4eeadbc
MashaMartin <59993828+MashaMartin@users.noreply.github.com>
2022-08-21 20:12:54 +0200

    Add files via upload

---
+++ src/arrayproject/arrayclass.java
@@ -0,0 +1,19 @@
+package arrayproject;
+
+public class arrayclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] newArr = new String[5];
+		newArr[0] = "Hello";
+		newArr[1] = "Hey";
+		newArr[2] = "hi";
+		newArr[3] = "Martin";
+		newArr[4] = "Masha";
+		
+		String x = newArr[4];
+		System.out.println(x);
+
+	}
+
+}
